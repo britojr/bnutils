09bf371ed4418e8eac8dd6c21c6cbe072ee60b89
Joao Brito <joaojr.89@gmail.com>
2018-04-09 17:51:31 -0300

    bif: use actual varnames on test

--- bif/bif_test.go
+++ bif/bif_test.go
@@ -7,14 +7,27 @@ import (
 )
 
 func TestBifStruct(t *testing.T) {
+	vs := []*vars.Var{
+		vars.New(0, 3, "Akt", false),
+		vars.New(1, 3, "Erk", false),
+		vars.New(2, 3, "Jnk", false),
+		vars.New(3, 3, "Mek", false),
+		vars.New(4, 3, "P38", false),
+		vars.New(5, 3, "PIP2", false),
+		vars.New(6, 3, "PIP3", false),
+		vars.New(7, 3, "PKA", false),
+		vars.New(8, 3, "PKC", false),
+		vars.New(9, 3, "Plcg", false),
+		vars.New(10, 3, "Raf", false),
+	}
 	cases := []struct {
 		fname                   string
 		roots, leafs, internals vars.VarList
 	}{{
 		"sachs.bif",
-		vars.NewList([]int{8, 9}, []int{3}),
-		vars.NewList([]int{0, 2, 4, 5}, []int{3}),
-		vars.NewList([]int{1, 3, 6, 7, 10}, []int{3}),
+		vars.VarList{vs[8], vs[9]},
+		vars.VarList{vs[0], vs[2], vs[4], vs[5]},
+		vars.VarList{vs[1], vs[3], vs[6], vs[7], vs[10]},
 	}}
 	for _, tt := range cases {
 		got := ParseStruct(tt.fname)
